ac82a1f6a0c4ac4152fabc0a6f74ab590596e7a7
James Liu <james321456987@gmail.com>
2021-07-15 20:36:09 +0800

    added door

--- src/scenes/Scene1.js
+++ src/scenes/Scene1.js
@@ -8,24 +8,42 @@ class Scene1 extends Phaser.Scene{
     }
     preload() {
         this.load.image('scene1_bg', './assets/background.png');
-        this.load.image('scene2_bg', './assets/background_2.png');
+        this.load.image('scene2_bg', './assets/scene_b.png');
+        this.load.image('scene3_bg', './assets/scene_c.png');
+        this.load.image('scene4_bg', './assets/scene_d.png');
         this.load.image('floor', './assets/floor.png');
         this.load.image('teddy', './assets/teddy.png');
+        this.load.image('door', './assets/door.png');
     }
 
     create() {
-        this.physics.world.setBounds(0, 0, 1572, 1312);
+        this.bg1 = this.add.image(0, 0, 'scene1_bg').setOrigin(0, 0);
+        this.cameras.main.setBounds(0, 0, this.bg1.displayWidth, this.bg1.displayHeight);
+        w = this.bg1.width;
+        h = this.bg1.height;
+        console.log(w, h);
+        this.physics.world.setBounds(0, 0, w, h);
 
         let platforms = this.physics.add.staticGroup();
-        this.w = this.cameras.main.width;
-        this.h = this.cameras.main.height;
-
         
-        this.bg1 = this.add.image(0, 0, 'scene1_bg').setOrigin(0, 0);
         this.floor = this.physics.add.image(0, 1312, 'floor').setOrigin(0, 1);
         
+        this.floor2 = this.physics.add.image(0, 500, 'floor').setOrigin(0, 1).setScale(0.1);
+        this.floor2.setImmovable(true);
+        this.floor2.body.allowGravity = false;
+
         this.floor.setCollideWorldBounds(true);
-        this.cameras.main.setBounds(0, 0, this.bg1.displayWidth, this.bg1.displayHeight);
+
+        
+        this.door1 = this.physics.add.image(0 + borderPadding, h-this.floor.height,'door')
+            .setOrigin(0,1).setScale(0.3);
+        this.door2 = this.physics.add.image(w - borderPadding, h-this.floor.height,'door')
+            .setOrigin(1,1).setScale(0.3);
+        
+        this.door1.setImmovable(true);
+        this.door1.body.allowGravity = false;
+        this.door2.setImmovable(true);
+        this.door2.body.allowGravity = false;
 
         this.player = new Teddy(this,0,0,'teddy').setOrigin(0,0);
         this.cameras.main.startFollow(this.player);
@@ -37,7 +55,6 @@ class Scene1 extends Phaser.Scene{
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
 
         this.gameOver = false;
-        var spawn = true;
         //this.physics.add.overlap(this.p1Police,this.truckGroup,this.crash,null,this);
 
         this.physics.add.collider(this.player, this.floor);
